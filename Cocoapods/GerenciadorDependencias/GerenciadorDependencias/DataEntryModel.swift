17e703a43bf73bf25e0fccfaa81fc2f8d3eef8ef
alessandrop76 <alessandrodigima@gmail.com>
2023-09-19 15:42:19 +0100

    Gerenciadores de Dependências: Cocoapods - SPM - Carthage

---
+++ Cocoapods/GerenciadorDependencias/GerenciadorDependencias/DataEntryModel.swift
@@ -0,0 +1,14 @@
+//
+//  DataEntryModel.swift
+//  GerenciadorDependencias
+//
+//  Created by alessandro pereira on 14/09/2023.
+//
+
+import Foundation
+import Charts
+
+struct DataEntryModel: Codable {
+    var x: Double
+    var y: Double
+}
